d4451e4e6a0147e349876a5e8a3c36d1bc4cf719
nersaview30business-cyber <nersaview30business@gmail.com>
2025-10-26 15:43:49 +0700

    Updated tooltip.tsx

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -17,7 +17,7 @@ export const Tooltip = ({ children, content }: TooltipProps): JSX.Element => {
         {children}
       </div>
       {isVisible && (
-        <div className="absolute left-full ml-3 top-1/2 -translate-y-1/2 z-50 px-6 py-3 bg-white text-[#1b1d23] text-xl rounded-md whitespace-nowrap shadow-lg border-2 border-[#e0e2e7] [font-family:'Inter',Helvetica] font-bold">
+        <div className="absolute left-full ml-3 top-1/2 -translate-y-1/2 z-50 px-3 py-1.5 bg-white text-[#1b1d23] text-xl rounded-md whitespace-nowrap shadow-lg border-2 border-[#e0e2e7] [font-family:'Inter',Helvetica] font-bold">
           {content}
           <div className="absolute right-full top-1/2 -translate-y-1/2 border-8 border-transparent border-r-white" style={{ filter: 'drop-shadow(-1px 0 0 #e0e2e7)' }} />
         </div>
